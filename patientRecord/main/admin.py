9412bcf7de9aeb5f3fbbb8330206948c43548797
Tooni <prashanth04@gmail.com>
2022-10-03 01:23:52 +0530

    Implemented Database Models, Admin Model, added required features.

--- patientRecord/main/admin.py
+++ patientRecord/main/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from .models import Pateint
 # Register your models here.
+
+
+class PatientAdmin(admin.ModelAdmin):
+    list_display = ('full_name', 'email', 'mobile_no',
+                    'detail', 'visit_date', 'next_visit_date')
+    search_fields = ('full_name', 'email', 'mobile_no',)
+
+
+admin.site.register(Pateint, PatientAdmin)
