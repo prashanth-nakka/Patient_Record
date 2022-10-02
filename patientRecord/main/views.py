497cfe7446aa15977adb7c980b06c1a8953a00c5
Tooni <prashanth04@gmail.com>
2022-10-03 02:10:14 +0530

    Implemented Home Page, Business logic in views.py, created urls.py

--- patientRecord/main/views.py
+++ patientRecord/main/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from .models import Pateint
 # Create your views here.
+
+
+def home(request):
+    data = Pateint.objects.all()
+    context = {'data': data}
+    return render(request, 'home.html', context)
