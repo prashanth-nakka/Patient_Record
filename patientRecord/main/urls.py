436be5c6e0eddaa002e025791b4086f568b06262
Tooni <prashanth04@gmail.com>
2022-10-03 03:03:44 +0530

    Add Patient Form, Business Logic,POST method, add-patient.html

--- patientRecord/main/urls.py
+++ patientRecord/main/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 from main import views
 urlpatterns = [
     path('', views.home, name='home'),
+    path('add-patient', views.add_patient, name='add-patient'),
 ]
--- patientRecord/main/views.py
+++ patientRecord/main/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from django.contrib import messages
 from .models import Pateint
+from .forms import PatientForm
 # Create your views here.
 
 
@@ -7,3 +9,15 @@ def home(request):
     data = Pateint.objects.all()
     context = {'data': data}
     return render(request, 'home.html', context)
+
+# ADD PATIENT
+
+
+def add_patient(request):
+    form = PatientForm
+    if request.method == 'POST':
+        saveForm = PatientForm(request.POST)
+        if saveForm.is_valid():
+            saveForm.save()
+            messages.success(request, "Pateint Added Successfully!")
+    return render(request, 'add-patient.html', {'form': form})
