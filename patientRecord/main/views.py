78a3b630b325e61f7b300b7913ecc667f606ee31
Tooni <prashanth04@gmail.com>
2022-10-03 23:27:41 +0530

    Implemented Update Business logic, update-patient.html, urls.py

--- patientRecord/main/views.py
+++ patientRecord/main/views.py
@@ -19,5 +19,19 @@ def add_patient(request):
         saveForm = PatientForm(request.POST)
         if saveForm.is_valid():
             saveForm.save()
-            messages.success(request, "Pateint Added Successfully!")
+            messages.success(request, "Pateint Details Added Successfully!")
     return render(request, 'add-patient.html', {'form': form})
+
+
+# UPDATE PATIENT
+def update_patient(request, id):
+    patient_id = Pateint.objects.get(id=id)
+    if request.method == 'POST':
+        saveForm = PatientForm(request.POST, instance=patient_id)
+        if saveForm.is_valid():
+            saveForm.save()
+            messages.success(request, "Patient Details Updated Successfully!")
+        else:
+            messages.error(request, "Patient Details Not Found!")
+    form = PatientForm(instance=patient_id)
+    return render(request, 'update-patient.html', {'form': form})
